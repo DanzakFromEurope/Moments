2783d61b1f227bacf0cc77b2c2b0517c794738d1
DanzakFromEurope <49127534+DanzakFromEurope@users.noreply.github.com>
2020-12-05 16:43:28 +0100

    User registration and login
    Firebase EmailPassword authentication

--- app/src/main/java/com/moments/MainActivity.java
+++ app/src/main/java/com/moments/MainActivity.java
@@ -1,14 +1,89 @@
 package com.moments;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button login;
+    private Button register;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.loginSubmit);
+        register = findViewById(R.id.register);
+        editTextEmail = findViewById(R.id.editTextLoginEmail);
+        editTextPassword = findViewById(R.id.editTextLoginPassword);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter your details", Toast.LENGTH_SHORT).show();
+                } else {
+                    loginUser(email, password);
+                }
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("LoginStatus", "signInWithEmail:success");
+                            Toast.makeText(getApplicationContext(), "Successfully logged in", Toast.LENGTH_SHORT).show();
+
+                            startActivity(new Intent(getApplicationContext(), WallActivity.class));
+
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("LoginStatus", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Unable to login", Toast.LENGTH_SHORT).show();
+//                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/moments/RegisterActivity.java
@@ -0,0 +1,80 @@
+package com.moments;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button register;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        editTextEmail = findViewById(R.id.editTextRegisterEmail);
+        editTextPassword = findViewById(R.id.editTextRegisterPassword);
+        register = findViewById(R.id.register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter your details", Toast.LENGTH_SHORT).show();
+                } else if (password.length() < 8) {
+                    Toast.makeText(getApplicationContext(), "Password is to short", Toast.LENGTH_SHORT).show();
+                } else {
+                    registerUser(email, password);
+                }
+            }
+        });
+    }
+
+    private void registerUser(String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("RegisterStatus", "createUserWithEmail:success");
+                    Toast.makeText(getApplicationContext(), "Successfully registered", Toast.LENGTH_SHORT).show();
+
+                    startActivity(new Intent(getApplicationContext(), WallActivity.class));
+                    finish();
+//                    FirebaseUser user = mAuth.getCurrentUser();
+//                    updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("RegisterStatus", "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(getApplicationContext(), "Unable to register", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+//                    updateUI(null);
+                }
+            }
+        });
+    }
+}
