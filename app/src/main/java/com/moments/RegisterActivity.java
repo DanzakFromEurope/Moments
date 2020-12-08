bbc7d9b47dd55abb876138f7b370a3b86d95d4bf
DanzakFromEurope <49127534+DanzakFromEurope@users.noreply.github.com>
2020-12-08 12:28:31 +0100

    Updating Registration process
    Repeat password
    Saving username, email and password
    Trying task management

--- app/src/main/java/com/moments/RegisterActivity.java
+++ app/src/main/java/com/moments/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.moments;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -17,15 +18,23 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText editTextEmail;
+    private EditText editTextUsername;
     private EditText editTextPassword;
     private EditText getEditTextPasswordAgain;
     private Button register;
 
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference firebaseRootRef;
+
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,43 +42,61 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         editTextEmail = findViewById(R.id.editTextRegisterEmail);
+        editTextUsername = findViewById(R.id.editTextRegisterUsername);
         editTextPassword = findViewById(R.id.editTextRegisterPassword);
         getEditTextPasswordAgain = findViewById(R.id.editTextRegisterPasswordAgain);
         register = findViewById(R.id.registerSubmit);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseRootRef = FirebaseDatabase.getInstance().getReference();
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = editTextEmail.getText().toString();
+                String username = editTextUsername.getText().toString();
                 String password = editTextPassword.getText().toString();
                 String passwordAgain = getEditTextPasswordAgain.getText().toString();
 
-                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(username)) {
                     Toast.makeText(getApplicationContext(), "Enter your details", Toast.LENGTH_SHORT).show();
                 } else if (password.length() < 8) {
                     Toast.makeText(getApplicationContext(), "Password is to short", Toast.LENGTH_SHORT).show();
-                } else if (password != passwordAgain) {
+                } else if (!password.equals(passwordAgain)) {
                     Toast.makeText(getApplicationContext(), "Passwords don't match", Toast.LENGTH_SHORT).show();
                 } else {
-                    registerUser(email, password);
+                    registerUser(email, username, password);
                 }
             }
         });
     }
 
-    private void registerUser(String email, String password) {
+    private void registerUser(String email, String username, String password) {
         firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("RegisterStatus", "createUserWithEmail:success");
-                    Toast.makeText(getApplicationContext(), "Successfully registered", Toast.LENGTH_SHORT).show();
+                    HashMap<String, Object> map = new HashMap<>();
+                    map.put("email", email);
+                    map.put("username", username);
+                    map.put("id", firebaseAuth.getCurrentUser().getUid());
 
-                    startActivity(new Intent(getApplicationContext(), WallActivity.class));
-                    finish();
+                    firebaseRootRef.child("Users").child(firebaseAuth.getCurrentUser().getUid()).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("RegisterStatus", "createUserWithEmail:success");
+                                Toast.makeText(getApplicationContext(), "Successfully registered", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(getApplicationContext(), WallActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+                    });
+
+                    // Sign in success, update UI with the signed-in user's information
 //                    FirebaseUser user = mAuth.getCurrentUser();
 //                    updateUI(user);
                 } else {
